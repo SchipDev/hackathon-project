791ae67e1b87d6bafdd6a024fadbcb8df865ba7b
Hanna Murai <annmuray75@gmail.com>
2020-07-24 13:20:56 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+// import { Switch, Route } from "react-router-dom";
+// import Home from './components/Home'
+// import About from './components/About'
+// import Work from './components/Work'
+// import Contact from './components/Contact'
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <h1>Testing</h1>
-        </Switch>
+        {/* <Switch>
+          <Route exact path="/" render={props => <Home {...props} />} />
+          <Route exact path="/about" render={props => <About {...props} />} />
+          <Route exact path="/work" render={props => <Work {...props} />} />
+          <Route exact path="/work" render={props => <Contact {...props} />} />
+        </Switch> */}
       </div>
     );
   }
